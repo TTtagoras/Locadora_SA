ce3394b8a78312c2ff3ec958a9fa155bcbb2b8d1
Matheus Montagna <101183773+TTtagoras@users.noreply.github.com>
2022-05-11 10:19:53 -0300

    Update Frm_CadastroCliente.cs

--- Frm/Frm_CadastroCliente.cs
+++ Frm/Frm_CadastroCliente.cs
@@ -40,7 +40,6 @@ namespace LocadoraSA.Frm
         {
             try
             {
-                //mudei hein
                 this.funcionario = new Clientes();
                 this.funcionario.CPF = Txt_Cpf.Text;
                 this.funcionario.Nome = Txt_Nome.Text;
